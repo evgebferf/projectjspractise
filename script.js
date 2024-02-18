8dc406b87d810757d332ad6d759355f971ea7c38
Evgeniy <evgeniynext1337@gmail.com>
2024-02-18 22:13:55 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+"use strict"
+console.log(1)
+
+const obj={
+    name: "Jhon",
+    age: 25,
+    isMarried: false
+};
+
+console.log(obj["name"]);
+
+alert("hello");
+
+const category = "toys";
+console.log(`https:/someurl/${category}`);
