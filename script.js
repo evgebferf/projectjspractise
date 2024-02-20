236b0db629e7ed560d59845e640ea9e7ccbaa92b
Evgeniy <evgeniynext1337@gmail.com>
2024-02-20 22:00:38 +0300

    second task

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 "use strict"
-const numberOfFilms = +prompt('сколько', '');
+const numberOfFilms = prompt('сколько фильмов вы посмотрели?', '');
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -9,13 +9,38 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('one of the films'),
-      b = prompt('what is it rating?'),
-      c= prompt('one of the films'),
-      d = prompt('what is it rating?');
+for (let i = 0; i < 2; i++){
+    const a = prompt('Один из фильмов?'),
+          b = prompt ('Рейтинг?');
+    if ( a != null && b != null && a != '' && b != '' && a.length < 50){
+    personalMovieDB.movies[a] = b;
+    console.log ('Done');
+    } else {
+        console.log('Error');
+        i--;
+    }
+};
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+if (personalMovieDB.count < 10){
+    console.log('malo');}
+else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30){
+    console.log('norm');}
+else if (personalMovieDB.count > 30){
+    console.log ('kinoman');}
+else{
+    console.log ('Error');}
 
 console.log(personalMovieDB);
+
+// let j = 0;
+// let result = '';
+// for (let i = 1; i < 7; i++){
+//     while ( j < i ){
+//         result += "*";
+//         j++;
+//     }
+//     result += "\n";
+//     j = 0;
+// };
+// console.log( result );
 
